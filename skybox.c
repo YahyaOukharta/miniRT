d190a0a62054434177e3551b0629f51f9ac99e30
Yahya Oukharta <youkhart@e1r6p7.1337.ma>
2020-01-21 02:58:22 +0100

    update

--- skybox.c
+++ skybox.c
@@ -6,7 +6,7 @@
 /*   By: youkhart <youkhart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/20 22:04:57 by youkhart          #+#    #+#             */
-/*   Updated: 2020/01/21 01:35:52 by youkhart         ###   ########.fr       */
+/*   Updated: 2020/01/21 02:53:56 by youkhart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,12 @@ int			get_sky_color(int x, int y)
 {
 	int			index;
 	float		ry;
+	int			sb_x;
+	int			sb_y;
 
-	ry = ((t_cam *)((t_obj *)g_rt.curr_cam->content)->details)->rot.y;
-	index = (int)floor(y * g_rt.skybox.width + x);
-	return (mult_colors(g_rt.skybox.img_data[index], 0.3));
+	ry = -fmod(((t_cam *)((t_obj *)g_rt.curr_cam->content)->details)->rot.y, 2 * M_PI) * 0.4;
+	sb_x = (int)floor(((float)(x + ry * (float)g_rt.g_res.x) / (float)g_rt.g_res.x ) * (float)g_rt.skybox.width);
+	sb_y = (int)floor(((float)y / (float)g_rt.g_res.y) * (float)g_rt.skybox.height);
+	index = (int)floor(sb_y * g_rt.skybox.width + sb_x);
+	return (mult_colors(g_rt.skybox.img_data[index], 0.5));
 }
