3f858627d9dc23bdf468f6bddccbb3e52e5cb37e
Yahya Oukharta <youkhart@e1r6p7.1337.ma>
2020-01-22 01:03:56 +0100

    ...

--- rays_and_intersections/inter_sphere.c
+++ rays_and_intersections/inter_sphere.c
@@ -6,7 +6,7 @@
 /*   By: youkhart <youkhart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/26 07:37:03 by youkhart          #+#    #+#             */
-/*   Updated: 2020/01/20 21:38:29 by youkhart         ###   ########.fr       */
+/*   Updated: 2020/01/22 01:01:37 by youkhart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int				solve_quadratic_sp(float coeffs[3], float *t1, float *t2)
 {
 	float	discr;
-	//float	q;
+	float	q;
 
 	discr = coeffs[1] * coeffs[1] - 4 * coeffs[0] * coeffs[2];
 	if (discr < 0)
@@ -27,51 +27,13 @@ int				solve_quadratic_sp(float coeffs[3], float *t1, float *t2)
 	}
 	else
 	{
-		// q = (coeffs[1] > 0) ?
-		// 	(-0.5 * (coeffs[1] + sqrt(discr)))
-		// 	: (-0.5 * (coeffs[1] - sqrt(discr)));
-		// *t1 = q / coeffs[0];
-		// *t2 = coeffs[2] / q;
-		*t1 = (-coeffs[1] - sqrt(discr)) / (2 * coeffs[0]);
-		*t2 = (-coeffs[1] + sqrt(discr)) / (2 * coeffs[0]);
+		q = (coeffs[1] > 0) ?
+			(-0.5 * (coeffs[1] + sqrt(discr)))
+			: (-0.5 * (coeffs[1] - sqrt(discr)));
+		*t1 = q / coeffs[0];
+		*t2 = coeffs[2] / q;
 	}
 	return (1);
-}
-
-void			get_dist(float *t0, float *t1, float *t2, int is_inside)
-{
-	*t0 = 0;
-	//printf("t1 %.2f t2 %.2f\n", *t1, *t2);
-	if (is_inside)
-	{
-		if (*t1 > RAY_T_MIN)
-			*t0 = *t1;
-		else if (*t2 > RAY_T_MIN)
-			*t0 = *t2;
-	}
-	else if (*t1 > RAY_T_MIN)
-		*t0 = *t1;
-	else if (*t2 > RAY_T_MIN)
-		*t0 = *t2;
-}
-
-int				is_inside(float *t1, float *t2)
-{
-	float tmp;
-
-	if (*t1 > *t2)
-	{
-		tmp = *t1;
-		*t1 = *t2;
-		*t2 = tmp;
-	}
-	if (*t1 < 0.0 && *t2 > RAY_T_MIN)
-	{
-		//printf("t1 %.2f t2 %.2f", *t1, *t2);
-		return (0);
-	}
-	else
-		return (0);
 }
 
 int				bool_intersects_with_sphere(t_ray ray, t_obj *obj, float *dist)
@@ -89,7 +51,11 @@ int				bool_intersects_with_sphere(t_ray ray, t_obj *obj, float *dist)
 	coeffs[2] = vec_len2(tmp_ray.pos) - sphere->diameter * sphere->diameter;
 	if (!(solve_quadratic_sp(coeffs, &t[1], &t[2])))
 		return (0);
-	get_dist(&t[0], &t[1], &t[2], is_inside(&t[1], &t[2]));
+	t[0] = 0;
+	if (t[1] > RAY_T_MIN)
+		t[0] = t[1];
+	if (t[2] < t[1] && t[2] > RAY_T_MIN)
+		t[0] = t[2];
 	if (!t[0])
 		return (0);
 	if (dist)
@@ -111,8 +77,8 @@ t_intersection	*intersects_with_sphere(t_ray ray, t_obj *obj)
 	inter->point = vec_add(ray.pos, vec_mult(ray.dir, t));
 	inter->t = t;
 	inter->object_color = sphere->color;
-	inter->normal = vec_mult(vec_normalize(
-		vec_sub(vec_add(ray.pos, vec_mult(ray.dir, t)), sphere->pos)), -1);
+	inter->normal = vec_normalize(
+		vec_sub(vec_add(ray.pos, vec_mult(ray.dir, t)), sphere->pos));
 	inter->diffuse = 0.4;
 	inter->specular = 0.4;
 	inter->s_power = 20;
--- skybox.c
+++ skybox.c
@@ -6,7 +6,7 @@
 /*   By: youkhart <youkhart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/20 22:04:57 by youkhart          #+#    #+#             */
-/*   Updated: 2020/01/21 02:53:56 by youkhart         ###   ########.fr       */
+/*   Updated: 2020/01/21 20:33:24 by youkhart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,10 @@ int			get_sky_color(int x, int y)
 	int			sb_x;
 	int			sb_y;
 
+	return (0);
 	ry = -fmod(((t_cam *)((t_obj *)g_rt.curr_cam->content)->details)->rot.y, 2 * M_PI) * 0.4;
 	sb_x = (int)floor(((float)(x + ry * (float)g_rt.g_res.x) / (float)g_rt.g_res.x ) * (float)g_rt.skybox.width);
 	sb_y = (int)floor(((float)y / (float)g_rt.g_res.y) * (float)g_rt.skybox.height);
 	index = (int)floor(sb_y * g_rt.skybox.width + sb_x);
-	return (mult_colors(g_rt.skybox.img_data[index], 0.5));
+	return (mult_colors(g_rt.skybox.img_data[index], 0.4));
 }
